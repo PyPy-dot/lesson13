8dd00e0174c632ce1b665030864b28898abe30ee
PyPy-dot <nerosed131@gmail.com>
2025-01-27 20:16:41 +0500

    1

--- module_13_3.py
+++ module_13_3.py
@@ -1,11 +1,17 @@
 from aiogram import types, Dispatcher, Bot, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 import asyncio
 
 API_TOKEN = 'TOKEN'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+
+button1 = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.row(button1, button2)
 
 
 class UserState(StatesGroup):
@@ -14,7 +20,11 @@ class UserState(StatesGroup):
     weight = State()
 
 
-@dp.message_handler(text='Calories')
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Выбрать', reply_markup=kb)
+
+@dp.message_handler(text='Рассчитать')
 async def set_age(message):
     await message.answer('Введите свой возраст:')
     await UserState.age.set()
