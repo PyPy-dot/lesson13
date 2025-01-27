c267199fb3b0750f0288d225347f84dd826e5afc
PyPy-dot <nerosed131@gmail.com>
2025-01-27 13:17:49 +0500

    1

--- module_13_2.py
+++ module_13_2.py
@@ -10,11 +10,13 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(commands=['start'])
 async def start(message) -> None:
     print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
 
 
 @dp.message_handler()
 async def all_massages(message) -> None:
     print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
 
 
 if __name__ == '__main__':
