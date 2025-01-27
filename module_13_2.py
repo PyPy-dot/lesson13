c2a1324129142d77b6e70075005f5c525faaf0be
PyPy-dot <nerosed131@gmail.com>
2025-01-27 12:56:16 +0500

    1

---
+++ module_13_2.py
@@ -0,0 +1,21 @@
+from aiogram import types, Dispatcher, Bot, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+API_TOKEN = 'ТОКЕН'
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message) -> None:
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_massages(message) -> None:
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
